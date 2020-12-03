885e68bbb9969187502ed5e74170051eac929bbd
amy576 <amy.ho576@gmail.com>
2020-12-02 23:14:37 -0800

    got q2 after scoping x incorrectly; also fixed the inefficient method of repeating records

--- 2020/day3/day3.go
+++ 2020/day3/day3.go
@@ -29,13 +29,13 @@ func q1(filename string) int {
 
 	// the actual record pattern repeats many times
 	// we need x to be able to be 3x as large as y
-	for i, record := range records {
-		for z := 0; z < ((numLines * 3 / numCols)+1); z ++ {
-			records[i] += record
-		}
-	}
+	// for i, record := range records {
+	// 	for z := 0; z < ((numLines * 3 / numCols)+1); z ++ {
+	// 		records[i] += record
+	// 	}
+	// }
 
-	fmt.Println("successfully repeated patterns")
+	// fmt.Println("successfully repeated patterns")
 
 	for y := 0; y < numLines; y++ {
 		currentChar := records[y][x:x+1]
@@ -43,6 +43,9 @@ func q1(filename string) int {
 			trees ++
 		}
 		x+=3
+		if x >= numCols {
+			x = x - numCols
+		}
 	}
 
 	return trees
@@ -63,14 +66,13 @@ func q2(filename string) int {
 	scanner := bufio.NewScanner(f)
 
 	var trees []int
-	slopes := make(map[int]int)
-	slopes[1] = 1
-	slopes[3] = 1
-	slopes[5] = 1
-	slopes[7] = 1
-	slopes[1] = 2
-
-	x := 0
+	// var slopes [5][2]int
+	slopes := make(map[int][2]int)
+	slopes[1] = [2]int{1, 1}
+	slopes[2] = [2]int{3, 1}
+	slopes[3] = [2]int{5, 1}
+	slopes[4] = [2]int{7, 1}
+	slopes[5] = [2]int{1, 2}
 
 	var records []string
 	for scanner.Scan() {
@@ -83,28 +85,26 @@ func q2(filename string) int {
 	numCols := len(records[0])
 	fmt.Println("number of base characters in a line ", numCols)
 
-	for key, element := range slopes {
-		// the actual record pattern repeats many times
-		// we need x to be able to be key times as large as y
-		for i, record := range records {
-			for z := 0; z < ((numLines * key / numCols)+1); z ++ {
-				records[i] += record
-			}
-		}
-
-		fmt.Println("successfully repeated patterns")
-
+	for _, slope := range slopes {
+		x := 0
+		xIncr := slope[0]
+		yIncr := slope[1]
+		fmt.Println("current slope is ", xIncr, ", ", yIncr)
 		runTrees := 0
 
-		for y := 0; y < numLines; y+=element {
+		for y := 0; y < numLines; y+=yIncr {
 			currentChar := records[y][x:x+1]
 			if currentChar == "#" {
 				runTrees ++
 			}
-			x+=key
+			x+=xIncr
+			if x >= numCols {
+				x = x - numCols
+			}
 		}
 
 		trees = append(trees,runTrees)
+		fmt.Println(runTrees)
 	}
 
 	totalTrees := 1
