9e330d25f87d7f3fc670a68651c72db5f5b76b73
amy576 <amy.ho576@gmail.com>
2020-12-16 23:11:57 -0800

    completed part 2. i cannot think in 4d space

--- 2020/day17/day17.go
+++ 2020/day17/day17.go
@@ -10,6 +10,7 @@ type Cell struct {
 	x int
 	y int
 	z int
+	w int
 }
 
 var defaultState = "inactive"
@@ -28,31 +29,34 @@ func readInput(filename string) map[Cell]string {
 		for j, char := range row {
 			// initialize one row before and one row after
 			// initialize one column before and one after
-			for z := -1; z <= 1; z++ {
-				if i == 0 {
-					cells[Cell{i-1, j, z}] = defaultState
-				} else if i == len(lines) - 1 {
-					cells[Cell{i+1, j, z}] = defaultState
-				}
-	
-				if j == 0 {
+			for w := -1; w <= 1; w++ {
+				for z := -1; z <= 1; z++ {
 					if i == 0 {
-						cells[Cell{i-1, j-1, z}] = defaultState
+						cells[Cell{i-1, j, z, w}] = defaultState
 					} else if i == len(lines) - 1 {
-						cells[Cell{i+1, j-1, z}] = defaultState
+						cells[Cell{i+1, j, z, w}] = defaultState
 					}
-					cells[Cell{i, j-1, z}] = defaultState
-				} else if j == len(row) - 1 {
-					if i == 0 {
-						cells[Cell{i-1, j+1, z}] = defaultState
-					} else if i == len(lines) - 1 {
-						cells[Cell{i+1, j+1, z}] = defaultState
+		
+					if j == 0 {
+						if i == 0 {
+							cells[Cell{i-1, j-1, z, w}] = defaultState
+						} else if i == len(lines) - 1 {
+							cells[Cell{i+1, j-1, z, w}] = defaultState
+						}
+						cells[Cell{i, j-1, z, w}] = defaultState
+					} else if j == len(row) - 1 {
+						if i == 0 {
+							cells[Cell{i-1, j+1, z, w}] = defaultState
+						} else if i == len(lines) - 1 {
+							cells[Cell{i+1, j+1, z, w}] = defaultState
+						}
+						cells[Cell{i, j+1, z, w}] = defaultState
 					}
-					cells[Cell{i, j+1, z}] = defaultState
-				}
 
-				if z != 0 {
-					cells[Cell{i, j, z}] = defaultState
+					if z == 0 && w == 0 {
+					} else {
+						cells[Cell{i, j, z, w}] = defaultState
+					}
 				}
 			}
 
@@ -62,9 +66,8 @@ func readInput(filename string) map[Cell]string {
 			} else if string(char) == "#" {
 				state = "active"
 			}
-			cell := Cell{i, j, 0}
+			cell := Cell{i, j, 0, 0}
 			cells[cell] = state
-			// also initialize the adjacent z layers
 		}
 	}
 	return cells
@@ -87,7 +90,7 @@ func change(startGrid map[Cell]string) (map[Cell]string) {
 					if i == xCoord && j == yCoord && k == zCoord {
 					} else {
 						// fmt.Println(i, j, k)
-						adjacentCell := Cell{i, j, k}
+						adjacentCell := Cell{i, j, k, 0}
 						adjacentStatus, exists := startGrid[adjacentCell]
 						_, alreadyInitialized := endGrid[adjacentCell]
 						if exists {
@@ -105,6 +108,76 @@ func change(startGrid map[Cell]string) (map[Cell]string) {
 							// to at least one active cell, since those are the only
 							// cells that have any chance of flipping to active
 							endGrid[adjacentCell] = defaultState
+						}
+					}
+				}
+			}
+		}
+
+		// During a cycle, all cubes simultaneously change their state
+		// according to the following rules:
+		// If a cube is active and exactly 2 or 3 of its neighbors are
+		// also active, the cube remains active. Otherwise, the cube
+		// becomes inactive.
+		// If a cube is inactive but exactly 3 of its neighbors are
+		// active, the cube becomes active. Otherwise, the cube remains
+		// inactive.
+		if state == "active" {
+			if adjacent == 2 || adjacent == 3 {
+				endGrid[coordinates] = "active"
+			} else {
+				endGrid[coordinates] = "inactive"
+			}
+		} else if state == "inactive" {
+			if adjacent == 3 {
+				endGrid[coordinates] = "active"
+			} else {
+				endGrid[coordinates] = "inactive"
+			}
+		}
+
+	}
+
+	return endGrid
+}
+
+func change4d(startGrid map[Cell]string) (map[Cell]string) {
+	endGrid := make(map[Cell]string)
+
+	for coordinates, state := range startGrid {
+		// Each cube only ever considers its neighbors: any of
+		// the 26 other cubes where any of their coordinates differ
+		// by at most 1. 
+		xCoord, yCoord, zCoord, wCoord := coordinates.x, coordinates.y, coordinates.z, coordinates.w
+		var adjacent int
+		// fmt.Println("neighbors of ", xCoord, yCoord, zCoord)
+		for i := xCoord - 1; i <= xCoord + 1; i++ {
+			for j := yCoord - 1; j <= yCoord + 1; j++ {
+				for k := zCoord - 1; k <= zCoord + 1; k++ {
+					for l := wCoord - 1; l <= wCoord + 1; l++ {
+						// skip this actual coordinate
+						if i == xCoord && j == yCoord && k == zCoord && l == wCoord {
+						} else {
+							// fmt.Println(i, j, k)
+							adjacentCell := Cell{i, j, k, l}
+							adjacentStatus, exists := startGrid[adjacentCell]
+							_, alreadyInitialized := endGrid[adjacentCell]
+							if exists {
+								if adjacentStatus == "active" {
+									adjacent++
+								}
+							} else if alreadyInitialized {
+							// } else if state == "active" {
+							} else {
+								// the space is technically infinite
+								// if we come across something where there is no
+								// coordinate there in the 26 adjacent cells, we
+								// need to initialize one as an inactive cell
+								// we should only need to do this for cells adjacent
+								// to at least one active cell, since those are the only
+								// cells that have any chance of flipping to active
+								endGrid[adjacentCell] = defaultState
+							}
 						}
 					}
 				}
@@ -160,6 +233,18 @@ func q1(grid map[Cell]string, numIter int) int {
 	return countActive(runGrid)
 }
 
+func q2(grid map[Cell]string, numIter int) int {
+	runGrid := grid
+	for cycle := 0; cycle < numIter; cycle++ {
+		runGrid = change4d(runGrid)
+		fmt.Println(cycle)
+		fmt.Println(countActive(runGrid))
+		// fmt.Println(runGrid)
+	}
+
+	return countActive(runGrid)
+}
+
 func main() {
 	filename := "day17_input.csv"
 	startingCells := readInput(filename)
@@ -171,7 +256,9 @@ func main() {
 	q1 := q1(startingCells, iterations)
 	fmt.Println("part 1: ", q1)
 
-	// Given your starting numbers, what will be the 30000000th number spoken? 
-	// q2 := q2(fieldValues, validTickets, myTicket)
-	// fmt.Println("part 2: ", q2)
+	// Starting with your given initial configuration, simulate six cycles
+	// in a 4-dimensional space. How many cubes are left in the active state
+	// after the sixth cycle? 
+	q2 := q2(startingCells, iterations)
+	fmt.Println("part 2: ", q2)
 }
