171148a1ae272082f4bb4b2c81830f7d7ad836f4
amy576 <amy.ho576@gmail.com>
2020-12-19 03:21:01 -0800

    attempted regex for the first time. completed day 19 part 1.

--- 2020/day19/day19.go
+++ 2020/day19/day19.go
@@ -9,12 +9,14 @@ import (
 	"regexp"
 )
 
-func readInput(filename string) (map[string][][]string, []string) {
+var dig = regexp.MustCompile("[0-9]+")
+
+func readInput(filename string) (map[string]string, []string) {
 	f, _ := os.Open(filename)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	var messages []string
-	rules := make(map[string][][]string)
+	rules := make(map[string]string)
 
 	var lines []string
 	for scanner.Scan() {
@@ -29,97 +31,67 @@ func readInput(filename string) (map[string][][]string, []string) {
 			messages = append(messages, line)
 		} else if strings.Contains(line, ":") {
 			splits := strings.Split(line, ": ")
-			var multi [][]string
-			for _, x := range strings.Split(splits[1], " | ") {
-				var single []string
-				for _, y := range strings.Split(x, " ") {
-					single = append(single, strings.Trim(y, string('"')))
-				}
-				multi = append(multi, single)
-			}
-			rules[splits[0]] = multi
+			rules[splits[0]] = strings.Trim(splits[1], string('"'))
 		}
 	}
 	return rules, messages
 }
 
-func turnIntoString(rule [][]string) string {
-	var ruleString string
-	for i, ruleSet := range rule {
-		ruleString += "("
-		ruleString += strings.Join(ruleSet, " ")
-		ruleString += ")"
-		if i < len(rule) - 1 {
-			ruleString += " | "
-		}
-	}
-
-	return ruleString
-}
-
 // start at rule 0 and start replacing
-func solveForRules(rules map[string][][]string, end int) []string {
-	endRule := rules[strconv.Itoa(end)] // this is a [][]string
-	re := regexp.MustCompile("[0-9]+")
-
-	// turn each number within endRule into a non-number
-	containsNumber := re.FindAllString(turnIntoString(endRule), -1)
+func rulesToRegex(rules map[string]string, end int) string {
+	endRule := rules[strconv.Itoa(end)]
+	containsNumber := dig.FindAllString(endRule, -1)
+	// fmt.Println("start with", endRule)
 	for len(containsNumber) > 0 {
-		for i, ruleSet := range endRule {		 // ruleSet is a []string
-			for j, number := range ruleSet {
-				initialRuleSet := make([]string, len(ruleSet))
-				copy(initialRuleSet, ruleSet)
-				if number == "a" || number == "b" {
+		for i, elem := range strings.Split(endRule, " ") {
+			element := strings.Trim(elem, "()")
+			if len(dig.FindAllString(elem, -1)) > 0 {
+				// fmt.Println(element, ":", rules[element])
+				if strings.Contains(rules[element], "|") {
+					if i == 0 {
+						endRule = strings.Replace(endRule, element + " ", "(" + rules[element] + ") ", 1)
+					} else if i == len(strings.Split(endRule, " ")) - 1 {
+						endRule = strings.Replace(endRule, " " + element, " (" + rules[element] + ")", 1)
+					} else {
+						endRule = strings.Replace(endRule, " " + element + " ", " (" + rules[element] + ") ", 1)
+						endRule = strings.Replace(endRule, "(" + element + " ", "((" + rules[element] + ") ", 1)
+						endRule = strings.Replace(endRule, " " + element + ")", " (" + rules[element] + "))", 1)
+					}
 				} else {
-					replace := rules[number] // replace is a [][]string
-
-					// replace the current ruleSet using replace[0]
-					overwriteRule := make([]string, len(ruleSet[:j]))
-					copy(overwriteRule, ruleSet[:j])
-					// endRule[i] = ruleSet[:j]
-					for _, x := range replace[0] {
-						overwriteRule = append(overwriteRule, x)
+					if i == 0 {
+						endRule = strings.Replace(endRule, element + " ", rules[element] + " ", 1)
+					} else if i == len(strings.Split(endRule, " ")) - 1 {
+						endRule = strings.Replace(endRule, " " + element, " " + rules[element], 1)
+					} else {
+						endRule = strings.Replace(endRule, " " + element + " ", " " + rules[element] + " ", 1)
+						endRule = strings.Replace(endRule, "(" + element + " ", "(" + rules[element] + " ", 1)
+						endRule = strings.Replace(endRule, " " + element + ")", " " + rules[element] + ")", 1)
 					}
-					for _, y := range initialRuleSet[j+1:] {
-						overwriteRule = append(overwriteRule, y)
-					}
-					endRule[i] = overwriteRule
-
-					// if len(replace) > 1 then we need to append a new rule to endRule
-					if len(replace) > 1 {
-						newRule := make([]string, len(ruleSet[:j]))
-						copy(newRule, ruleSet[:j])
-						for _, x := range replace[1] {
-							newRule = append(newRule, x)
-						}
-						for _, y := range initialRuleSet[j+1:] {
-							newRule = append(newRule, y)
-						}
-						endRule = append(endRule, newRule)
-					}
-
-					break
 				}
 			}
-			containsNumber = re.FindAllString(turnIntoString(endRule), -1)
-			// fmt.Println("current endRule array", endRule)
+			// fmt.Println("replace element and now we have", endRule)
 		}
+		containsNumber = dig.FindAllString(endRule, -1)
+		// fmt.Println(len(endRule))
+		// fmt.Println(endRule)
+		// fmt.Println(regexp.MustCompile(strings.ReplaceAll(endRule, " ", "")))
 	}
 
-	var finalStrings []string
-	for _, rules := range endRule { // rules will be a []string
-		finalStrings = append(finalStrings, strings.Join(rules, ""))
-	}
-	return finalStrings
+	// fmt.Println(endRule)
+
+	cleanedRule := strings.ReplaceAll(endRule, " ", "")
+	// fmt.Println(cleanedRule)
+	return cleanedRule
 }
 
-func q1(rules map[string][][]string, messages []string, ruleNum int) int {
-	validStrings := solveForRules(rules, ruleNum)
+func q1(rules map[string]string, messages []string, ruleNum int) int {
+	re := regexp.MustCompile(rulesToRegex(rules, ruleNum))
 	// iterate over messages, iterate over rules[0] and increment match number if they match
 	var valid int
 	for _, message := range messages {
-		for _, validString := range validStrings {
-			if message == validString {
+		found := re.FindAllString(message, -1)
+		if len(found) == 1 {
+			if len(found[0]) == len(message) {
 				valid++
 			}
 		}
